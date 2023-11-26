c876a81bcbedc47e4b9b22b8ac9a3dab8afb0fb1
Kevin Lacavalerie <kevin.lacavalerie@zenika.com>
2023-11-26 18:05:34 +0100

    add workaround to swagger limitation

---
+++ src/main/kotlin/com/alexandria/papyrus/config/OpenAPIConfiguration.kt
@@ -0,0 +1,22 @@
+package com.alexandria.papyrus.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+
+@Configuration
+class OpenAPIConfiguration {
+    /**
+     * This bean is used to configure the JSON media type to be application/octet-stream to work around a bug/limitation in Swagger UI.
+     * Our application allows posting multipart with parts that have different content-types. Swagger UI does let you specify the content-type.
+     * https://github.com/swagger-api/swagger-ui/issues/6462
+     */
+    @Bean
+    fun octetStreamJsonConverter(): MappingJackson2HttpMessageConverter {
+        val converter = MappingJackson2HttpMessageConverter()
+        converter.supportedMediaTypes =
+            listOf(MediaType("application", "octet-stream"))
+        return converter
+    }
+}
